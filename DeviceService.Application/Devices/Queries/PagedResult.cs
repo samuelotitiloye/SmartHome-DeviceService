e473520a3e222bff395d32eb3a64dad917e2ceed
Samuel Titiloye <samuelt@Samuels-MacBook-Pro.local>
2025-11-20 23:28:21 -0600

    fix: add PagedResult and Queries folder missing from repo

---
+++ DeviceService.Application/Devices/Queries/PagedResult.cs
@@ -0,0 +1,11 @@
+namespace DeviceService.Application.Devices.Queries
+{
+    public class PagedResult<T>
+    {
+        public IEnumerable<T> Items { get; set; } = Enumerable.Empty<T>();
+        public int Page { get;set; }
+        public int PageSize { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalItems / PageSize);
+    }
+}
